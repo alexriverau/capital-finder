9d9819c7377d19d522593f7a2340d9626f5f7a17
Alejandro Rivera <alexrivera78@gmail.com>
2022-11-27 20:35:03 -0600

    lab16 completed

---
+++ README.md
@@ -0,0 +1,22 @@
+# Lab16 - Serverless Functions
+
+---
+
+### Project: capital-finder
+### Author: Alejandro Rivera
+
+---
+
+### Features
+ * Create a serverless function following Vercel’s get-started directions that handles two kinds of queries:
+   * Handle a GET http request with a given country name that responds with its capital.
+   * Handle a GET http request with a given capital that responds with its country.
+
+### How to initialize/run application
+
+http://capital-finder-alejandro-rivera.vercel.app/api/capital_finder
+
+### Example urls for deployed function
+
+* Country: http://capital-finder-alejandro-rivera.vercel.app/api/capital_finder?country=chile
+* Capital: http://capital-finder-alejandro-rivera.vercel.app/api/capital_finder?capital=santiago
--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,11 +1,36 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from urllib import parse
+import requests
+
 
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%d-%m-%Y %H:%M:%S')).encode())
-    return
+        if 'country' in dic:
+            url = 'https://restcountries.com/v2/name/'
+            r = requests.get(url + dic['country'])
+            data = r.json()
+            country = data[0]['name']
+            capital = data[0]['capital']
+            message = f'The capital of {country} is {capital}.'
+
+        elif "capital" in dic:
+            url = 'https://restcountries.com/v2/capital/'
+            r = requests.get(url + dic["capital"])
+            data = r.json()
+            country = data[0]['name']
+            capital = data[0]['capital']
+            message = f'{capital} is the capital of {country}.'
+
+        else:
+            message = 'Error. Invalid Request.'
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+        return
